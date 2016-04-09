d906d696f6bb67aa870eba99d9a9dc623327e249
Ivan Towlson <github@hestia.cc>
2016-04-09 12:55:50 +1200

    Additional Bevaning

--- Samples/PrimitiveObsession/PrimitiveObsession/Checkers.cs
+++ Samples/PrimitiveObsession/PrimitiveObsession/Checkers.cs
@@ -8,6 +8,46 @@ namespace PrimitiveObsession
 {
     public static class Checkers
     {
+        public static void MakeRequest()
+        {
+            var wr = System.Net.WebRequest.CreateHttp("http://example.com");
+            wr.Timeout = 10 * 1000;  // 10 seconds (10000 ms)?  10000 seconds?  1/10000 second (10000 ticks)?
+        }
+
+        public static Grid CreateGrid()
+        {
+            return GridProvider.Create(
+                true,
+                false,
+                false,
+                true,
+                true,
+                20,
+                10,
+                70,
+                50,
+                false,
+                false);
+        }
+
+        public static void EnterMarsOrbit()
+        {
+            decimal distanceFromMars = Astrophysics.GetDistanceFromMars();
+            decimal distanceRequiredToDecelerate = Engine.GetDecelerationDistance();
+            decimal orbitalHeight = FlightPlanning.GetOrbitalHeight();
+
+            if (distanceRequiredToDecelerate < distanceFromMars - orbitalHeight)
+            {
+                Engine.FireTheRetroThrustersCaptain();
+            }
+        }
+
+        public static void StopIt_StopItNow()
+        {
+            string timeSeriesId = "STORED_TS_SPLINE_COUNTS";
+            string whatDoesThisEvenMean = timeSeriesId + "_CUMULATIVE";
+        }
+
         public static void NothingCanPossiblyGoWrong()
         {
             // Which is it?
@@ -34,6 +74,45 @@ namespace PrimitiveObsession
 
         public static void SetDecelerationPoint2(Distance distanceFromMars)
         {
+            Console.WriteLine($"Distance in feet is {distanceFromMars:ft}");
+        }
+
+        public class Grid { }
+
+        private static class GridProvider
+        {
+            public static Grid Create(
+                bool showVerticalGridlines,
+                bool showHorizontalGridlines,
+                bool invertColours,
+                bool showHeaderRow,
+                bool showAlternateRowColours,
+                int cellMarginX,
+                int cellMarginY,
+                int minCellWidth,
+                int minCellHeight,
+                bool autoStretch,
+                bool autoWrap
+                )
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private static class Astrophysics
+        {
+            public static decimal GetDistanceFromMars() { return 0; }
+        }
+
+        private static class Engine
+        {
+            public static decimal GetDecelerationDistance() { return 0; }
+            public static decimal FireTheRetroThrustersCaptain() { return 0; }
+        }
+
+        private static class FlightPlanning
+        {
+            public static decimal GetOrbitalHeight() { return 0; }
         }
     }
 }
